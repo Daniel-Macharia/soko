89b56d56c6e4e90e226961408e97bf46b408ab57
Daniel-Macharia <ndungudanny444@gmail.com>
2024-10-04 11:51:43 +0300

    allow return to login from sign up using back button

--- app/src/main/java/com/example/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -75,7 +75,6 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(LoginActivity.this, SignUp.class);
                 startActivity(intent);
-                finishAffinity();
             }
         });
     }
